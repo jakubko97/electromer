8a4b05c538101efcd97f3b1d7804e8d56b3cbacf
Jakub Jančík <jja@digitalsystems.eu>
2021-03-10 12:31:11 +0100

    feat quick select in recent days - add clickable segments with last days to get data in graph

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -41,7 +41,7 @@ export class DashboardPage implements OnInit {
   ionViewWillEnter() {
   }
 
-  compareFn(e1: Electromer, e2: Electromer): boolean {
+  compareFn(e1: any, e2: any): boolean {
     return e1 && e2 ? e1.id === e2.id : e1 === e2;
   }
 
@@ -131,7 +131,7 @@ export class DashboardPage implements OnInit {
           dataPoints: this.dataPoints3
         }],
         slider: {
-          minimum: new Date("2020-03-02"),
+          minimum: new Date("2019-03-02"),
           maximum: new Date("2021-03-08")
         }
       }
@@ -158,15 +158,16 @@ export class DashboardPage implements OnInit {
     return this.authService.getAllElectromers()
       .subscribe(
         electromers => {
-          this.electromers = electromers as Electromer
+          this.electromers = JSON.parse(JSON.stringify(electromers))
+          this.electromers = this.cleanElectromersData()
           var el_id = null
           if (this.electromer == null) { //init data default na posledne 7 dni
             el_id = this.electromers[0].id
-             var ourDate = new Date();
-             var pastDate = ourDate.getDate() - 7;
-             ourDate.setDate(pastDate);
-             this.from_date = ourDate
-             this.to_date = new Date()
+            var ourDate = new Date();
+            var pastDate = ourDate.getDate() - 3;
+            ourDate.setDate(pastDate);
+            this.from_date = ourDate
+            this.to_date = new Date()
           } else {
             el_id = this.electromer.id
           }
@@ -188,6 +189,42 @@ export class DashboardPage implements OnInit {
         }
       )
   }
+
+  quickSelect(value) {
+    switch (value.detail.value) {
+      case "last_day": this.selectRecentDays(1)
+        break;
+      case "last_three": this.selectRecentDays(3)
+        break;
+      case "last_seven": this.selectRecentDays(7)
+        break;
+      case "last_ten": this.selectRecentDays(10)
+        break;
+      case 'default': ''
+        break;
+    }
+  }
+
+  selectRecentDays(lastDays) {
+    var ourDate = new Date();
+    var pastDate = ourDate.getDate() - lastDays;
+    ourDate.setDate(pastDate);
+    this.from_date = ourDate
+    this.to_date = new Date()
+    this.initGraph()
+  }
+
+  cleanElectromersData(){
+    var clean_array = []
+    for (let data of Object.entries(this.electromers)) { //data -> mapa 0 key, 1 value
+      if(data[1] != null){
+        clean_array.push(data[1])
+      }
+    }
+    return clean_array
+    ;
+  }
+
   parseDataInChart() {
     for (let data of Object.entries(this.selectedDataInRange)) { //data -> mapa 0 key, 1 value
       var time = data[1]['time']
@@ -207,8 +244,8 @@ export class DashboardPage implements OnInit {
   }
 
   ngOnInit() {
-    // this.getElectromers()
     this.initGraph()
+    this.user = this.authService.user
   }
 
 }
